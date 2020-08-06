6401e51d3ec7caab6ebdfa94da155c73f8656335
Will Seagar <w.seagar@gmail.com>
2020-08-06 18:33:20 +1200

    create web server

---
+++ index.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const hapi = require("@hapi/hapi");
+
+const port = 5000;
+
+async function run() {
+  const server = hapi.server({
+    port: port
+  });
+
+  server.route({
+    path: "/",
+    method: "GET",
+    handler: () => {
+      return { hello: "world"}
+    }
+  })
+
+  await server.start();
+  console.log("Server running on port" + port);
+
+}
+
+run();
